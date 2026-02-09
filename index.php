58b6a1e79018afa0423ef2e12162a6c10ef1cf58
woongnone <s2302992@usls.edu.ph>
2026-02-10 00:52:03 +0800

    updated index.php and added new files for admin

--- index.php
+++ index.php
@@ -7,35 +7,34 @@ require_once __DIR__ . '/config/config.php';
 $action = $_GET['action'] ?? 'home';
 
 switch ($action) {
+
     case 'home':
         require_once CONTROLLER_PATH . 'HomeController.php';
-        $controller = new HomeController();
-        $controller->index();
+        (new HomeController())->index();
         break;
-    
+
     case 'login':
         require_once CONTROLLER_PATH . 'AuthController.php';
-        $controller = new AuthController();
-        $controller->showLogin();
+        (new AuthController())->showLogin();
         break;
 
     case 'signup':
         require_once CONTROLLER_PATH . 'AuthController.php';
-        $controller = new AuthController();
-        $controller->showSignup();
-        break;
-        case 'signup':
-
-    require_once CONTROLLER_PATH . 'AuthController.php';
-        $controller = new AuthController();
-        $controller->showDashboard();
+        (new AuthController())->showSignup();
         break;
 
+    case 'dashboard':
+        require_once CONTROLLER_PATH . 'AuthController.php';
+        (new AuthController())->showDashboard();
+        break;
+
+    case 'admin':
+        require_once CONTROLLER_PATH . 'AdminController.php';
+        (new AdminController())->adminDashboard();
+        break;
 
     default:
         require_once CONTROLLER_PATH . 'HomeController.php';
-        $controller = new HomeController();
-        $controller->index();
+        (new HomeController())->index();
         break;
 }
-?>
